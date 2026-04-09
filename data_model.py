946673385a0702dd7c798924a3f88493a83c8f32
Ankit Gadhwal <165482762+ankit-gadhwal@users.noreply.github.com>
2026-04-09 18:02:28 +0530

    Delete data_model.py

--- data_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-class NewStudent(BaseModel):
-    name: str
-    age: int
-
-class UpdateStudent(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = None
